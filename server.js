8eb6596d8067298dd35e5a80bf823c06a2cf56b3
Hashim. A <162900023+HashimAyamon@users.noreply.github.com>
2025-02-24 10:05:53 +0530

    updations done

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 
-const productRoutes = require('./routes/productRoutes'); 
-
+const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 dotenv.config();
 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use('/api/products', productRoutes);
+app.use("/api/products", productRoutes);
 
-app.get('/', (req, res) => {
-    res.send('Welcome!');
+app.get("/", (req, res) => {
+  res.send("Welcome!");
 });
 
-
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
