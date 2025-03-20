d2bc188969cab96635d233cae856f1ecc293cf72
Hashim. A <162900023+HashimAyamon@users.noreply.github.com>
2025-03-20 20:05:34 +0530

    updations made

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,23 +1,23 @@
-const express = require('express');
+const express = require("express");
 const {
-  getProducts,       // Renamed to match controller
-  getProductById,    // Renamed to match controller
+  getProducts, 
+  getProductById, 
   createProduct,
   updateProduct,
-  deleteProduct
-} = require('../controllers/productController');
+  deleteProduct,
+} = require("../controllers/productController");
 
-const { protect, adminOnly } = require('../middlewares/authMiddleware');
+const { protect, adminOnly } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-// Public Routes
-router.get('/', getProducts);
-router.get('/:id', getProductById);
 
-// Admin Only Routes
-router.post('/', protect, adminOnly, createProduct);
-router.put('/:id', protect, adminOnly, updateProduct);
-router.delete('/:id', protect, adminOnly, deleteProduct);
+router.get("/", getProducts);
+router.get("/:id", getProductById);
+
+
+router.post("/", protect, adminOnly, createProduct);
+router.put("/:id", protect, adminOnly, updateProduct);
+router.delete("/:id", protect, adminOnly, deleteProduct);
 
 module.exports = router;
